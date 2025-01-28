5abf118424e5b2974eaf5ddb4ef8079402acc7df
hotpup <chuncaleb0@gmail.com>
2025-01-28 05:54:20 +0000

    adding try catches

--- WebCrawl.java
+++ WebCrawl.java
@@ -3,27 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.MalformedInputException;
 
 public class WebCrawl {
     public static void main(String[] args) {
-        if(args.length != 2) {
+        if (args.length != 2) {
             System.out.println("Usage: java Webcrawler <url> <num_hops>");
             return;
         }
         String stringUrl = args[0];
-        int numHops = args[1];
-        
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-
-        int statusCode = connection.getResponseCode();
-        System.out.println(statusCode);
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while((line = reader.readLine()) != null) {
-            System.out.println(line);
+        int numHops;
+        try {
+            numHops = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            return;
         }
-        
+        try {
 
+            URL url = new URL(stringUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            int statusCode = connection.getResponseCode();
+            System.out.println(statusCode);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+            connection.disconnect();
+        } catch (MalformedInputException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
