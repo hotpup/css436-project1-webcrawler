562af2790e898f9f7ab1c3f7c6e5dbf06ced66d7
hotpup <chuncaleb0@gmail.com>
2025-01-28 06:28:32 +0000

    libraries added

--- WebCrawl.java
+++ WebCrawl.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
 import java.nio.charset.MalformedInputException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebCrawl {
     public static void main(String[] args) {
